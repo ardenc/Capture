2b23d2e3b6321a01e5810928c6688ecbe611da71
Arden Csolkovits <arden.csolkovits@gmail.com>
2021-06-04 08:51:36 -0400

    added styling to about section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
+//Global Styles
+import GlobalStyle from "./components/GlobalStyle";
 //import of pages
 import AboutUs from "./pages/AboutUs";
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <AboutUs />
     </div>
   );
