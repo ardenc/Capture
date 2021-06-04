e60bd6df5cbea9fed96d5256faf44d62b5077385
Arden Csolkovits <arden.csolkovits@gmail.com>
2021-06-04 10:42:45 -0400

    Added more styles

--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -9,6 +9,8 @@ const GlobalStyle = createGlobalStyle`
 
 body {
     background: #1b1b1b;
+    font-family: 'Inter', sans-serif;
+
 }
 
 button{
@@ -35,6 +37,7 @@ h3{
 }
 h4{
     font-weight: bold;
+    font-size: 2rem;
 }
 span{
     font-weight: bold;
